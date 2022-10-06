72ba7b0ca117e063af614ac95cf3d1b3040c9b88
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-10-06 21:45:31 +0500

    connected to db using sequelize

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { Sequelize } from 'sequelize';
+
+// put db into separate file
+
+const main = async () => {
+    const sequelize = new Sequelize('collector', 'root', '1234', {
+        host: 'localhost',
+        dialect: 'mysql',
+    });
+
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database: ', error);
+    }
+
+    sequelize.close();
+
+};
+
+main();
