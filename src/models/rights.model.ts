f047df7011f5e7d1bd4a7f7215f22976c736a25b
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-10-07 23:56:40 +0500

    role column from user model was moved into separate table

---
+++ src/models/rights.model.ts
@@ -0,0 +1,20 @@
+import { Sequelize, DataTypes } from 'sequelize';
+
+export default (sequelize: Sequelize) => {
+   
+    const Role = sequelize.define('role', {
+        id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            unique: true,
+        },
+        role: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
+
+    return Role;
+};
