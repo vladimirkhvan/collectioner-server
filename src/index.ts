10ef5ea8a68b187253ebecb3786dbbafcc46c54e
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-10-10 21:40:29 +0500

    resolvers and typeDefs were added / server start command was added

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import { db } from './models/index';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 
-
+import { resolvers } from './schema/Resolvers';
+import { typeDefs } from './schema/TypeDefs';
 
 const main = async () => {
     const app = express();
-    const server = new ApolloServer({typeDefs, resolvers});
-    server.applyMiddleware({app})
+    const server = new ApolloServer({ typeDefs, resolvers });
+    await server.start();
+    server.applyMiddleware({ app });
 
     try {
         await db.sequelize.sync();
