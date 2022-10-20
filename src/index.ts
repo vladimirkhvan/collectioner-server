00ce883f982a9984d2c964e9d1c0ea19df8fa3f4
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-10-20 10:53:08 +0500

    redis and cors were added

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,48 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 
 import { schema } from './schema/Schema';
+import cors from 'cors';
+
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import { redis } from './redis';
 
 const main = async () => {
     const app = express();
-    const server = new ApolloServer({ schema });
+    app.set('trust proxy', process.env.NODE_ENV !== 'production')
+
+    const RedisStore = connectRedis(session);
+
+    app.use(
+        cors<cors.CorsRequest>({
+            credentials: true,
+            origin: ['http://localhost:3000', 'https://studio.apollographql.com'],
+        }),
+    );
+    
+
+    const server = new ApolloServer({ schema, context: ({ req, res }) => ({ req, res }) });
+
+    app.use(
+        session({
+            store: new RedisStore({
+                client: redis,
+            }),
+            name: 'qid',
+            secret: '1234',
+            resave: false,
+            saveUninitialized: false,
+            cookie: {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                maxAge: 1000 * 60 * 60 * 24 * 365,
+                sameSite: "none",
+            },
+        }),
+    );
+
     await server.start();
+
     server.applyMiddleware({ app });
 
     try {
